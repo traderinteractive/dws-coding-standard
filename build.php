866726a03b44c4d8c59d77585198fca7bfa8a5ff
Jonathan Gaillard <jonathan.gaillard@dominionenterprises.com>
2013-04-17 20:06:27 -0400

    fix code coverage ignore tags not working with coveredelements

--- build.php
+++ build.php
@@ -19,7 +19,7 @@ if ($returnStatus !== 0) {
 }
 
 $xml = new SimpleXMLElement(file_get_contents('clover.xml'));
-foreach ($xml->xpath('//metrics') as $metric) {
+foreach ($xml->xpath('//file/metrics') as $metric) {
     if ((int)$metric['elements'] !== (int)$metric['coveredelements']) {
         file_put_contents('php://stderr', "Code coverage was NOT 100%\n");
         // exit(1); TODO: Get code coverage to 100%
