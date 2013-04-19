f5e1feef61317ba662c5c1ed3a9632f03307f355
Jonathan Gaillard <jonathan.gaillard@dominionenterprises.com>
2013-04-18 21:17:46 -0400

    Put note in output about code coverage not 100%

--- build.php
+++ build.php
@@ -21,8 +21,8 @@ if ($returnStatus !== 0) {
 $xml = new SimpleXMLElement(file_get_contents('clover.xml'));
 foreach ($xml->xpath('//file/metrics') as $metric) {
     if ((int)$metric['elements'] !== (int)$metric['coveredelements']) {
-        file_put_contents('php://stderr', "Code coverage was NOT 100%\n");
-        exit(0); //TODO: Get code coverage to 100%
+        file_put_contents('php://stderr', "Code coverage was NOT 100% but we need to get there ASAP\n");
+        exit(0);
     }
 }
 
