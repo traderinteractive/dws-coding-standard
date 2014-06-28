c9e18cc2f29e97682061b9e1b3b548882a9f2386
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-06-28 14:34:16 -0400

    Remove unnecessary arguments to composer install.
    We don't set these elsewhere and don't need to set them here.

--- build.php
+++ build.php
@@ -3,7 +3,7 @@
 chdir(__DIR__);
 
 $returnStatus = null;
-passthru('composer install --dev --no-interaction --prefer-source', $returnStatus);
+passthru('composer install --dev', $returnStatus);
 if ($returnStatus !== 0) {
     exit(1);
 }
